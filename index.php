36f7506e773102872fdbc70b5cc5f70f1f1c3ea8
Jake Nabasny <jnabasny@gmail.com>
2022-07-08 10:07:25 -0400

    UI improvements

--- index.php
+++ index.php
@@ -3,39 +3,18 @@
 $config = parse_ini_file("hosts.ini", true);
 
 
-
+include('header.php');
 ?>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>synopsis</title>
-<link rel="stylesheet" href="main.css">
-<link rel="stylesheet" href="css/all.css">
-<link rel="stylesheet" href="leaflet/leaflet.css"
-   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-   crossorigin=""/>
- <script src="leaflet/leaflet.js"
-   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-   crossorigin=""></script>
-</head>
-<body>
-<div class="navbar">
-  <a href="index.php"><i class="fas fa-globe-americas"></i>  Network</a>
-
-  <a href="host.php"><i class="fas fa-server"></i>  Host 1</a>
-  <a href="host.php"><i class="fas fa-server"></i>  Host 2</a>
-  <a href="host.php"><i class="fas fa-server"></i>  Host 3</a>
-</div>
 <div id="mapid"></div>
 <script>
 
 	var mymap = L.map('mapid').setView([39.100108495062166, -94.57162622176607], 5);
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam5hYmFzbnkiLCJhIjoiY2ttcGZuZzQ5MGJ1NjJ2bnNpNXBleHVjaSJ9.Px1mdjtmeW7XKFkE5mvclQ', {
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
 			'Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'jnabasny/cl59m1m0b001z14rtuiz9roge',
+		id: '',
 		tileSize: 512,
 		zoomOffset: -1
 	}).addTo(mymap);
